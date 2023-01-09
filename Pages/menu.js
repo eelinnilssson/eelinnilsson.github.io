3af98de4fc856b0f04c8c0799093565e2e7f1e96
eelinnilssson <elnilss@student.chalmers.se>
2023-01-09 14:17:42 +0100

    Hamburger Menu added

---
+++ Pages/menu.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector(".menu");
+let open;
+
+function openMenu() {
+    if (open) {
+        menu.style.display = "none";
+        open = false;
+    } else if (!open) {
+        menu.style.display = "block";
+        open = true;
+    }
+}
